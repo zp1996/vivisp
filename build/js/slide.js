2a57a21534e8277ba81ad4f5f28651c2f391d597
周鹏 <1179258516@qq.com>
2016-09-06 20:19:51 +0800

    add a slide

---
+++ build/js/slide.js
@@ -0,0 +1,1 @@
+"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(exports,"__esModule",{value:!0});var _lsgo=require("./lsgo"),_lsgo2=_interopRequireDefault(_lsgo);console.log(_lsgo2.default),exports.default=Slide;
---
+++ src/js/slide.js
@@ -0,0 +1,11 @@
+import $ from "./lsgo";
+
+console.log($);
+
+// function Slide () {
+
+// }
+// function getImg (img) {
+
+// }
+export default Slide;
